cb49e41a5202a5d0a0ad81a9359105a6d75a8d96
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-05-13 21:33:35 +0900

    feat: 상품 카테고리 추가

--- src/main/java/com/capstone/repository/queryrepository/ItemQueryRepository.java
+++ src/main/java/com/capstone/repository/queryrepository/ItemQueryRepository.java
@@ -1,8 +1,15 @@
 package com.capstone.repository.queryrepository;
 
+import com.capstone.domain.Gender;
+import com.capstone.domain.Item.Item;
 import com.capstone.dto.ItemDetailDto;
+import com.capstone.dto.ItemResponseDto;
 import com.capstone.dto.ItemReviewDto;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -23,7 +30,7 @@ public class ItemQueryRepository {
         return result;
     }
 
-    private ItemDetailDto findItemDetails(Long itemId) {
+    public ItemDetailDto findItemDetails(Long itemId) {
         return em.createQuery(
                         "select new com.capstone.dto.ItemDetailDto(i.company, i.image, i.itemName, i.price, i.siteUrl)" +
                                 " from Item i" +
@@ -32,7 +39,7 @@ public class ItemQueryRepository {
                 .getSingleResult();
     }
 
-    private List<ItemReviewDto> findItemReview(Long itemId) {
+    public List<ItemReviewDto> findItemReview(Long itemId) {
         return em.createQuery(
                         "select new com.capstone.dto.ItemReviewDto(m.username ,r.title, r.content, r.star)" +
                                 " from Review r" +
@@ -41,5 +48,50 @@ public class ItemQueryRepository {
                 .setParameter("itemId", itemId)
                 .getResultList();
 
+    }
+
+
+
+    public List<ItemResponseDto> findTopCategory(){
+        return em.createQuery(
+                "select new com.capstone.dto.ItemResponseDto(i.id, i.image, i.itemName, i.price, i.company)" +
+                        " from Item i" +
+                        " where TYPE(i) = Top", ItemResponseDto.class)
+                .getResultList();
+    }
+
+    public List<ItemResponseDto> findBottomCategory(){
+        return em.createQuery(
+                "select new com.capstone.dto.ItemResponseDto(i.id, i.image, i.itemName, i.price, i.company)" +
+                        " from Item i" +
+                        " where TYPE(i) = Bottom", ItemResponseDto.class)
+                .getResultList();
+    }
+
+    public List<ItemResponseDto> findManCategory(){
+        return em.createQuery(
+                        "select new com.capstone.dto.ItemResponseDto(i.id, i.image, i.itemName, i.price, i.company)" +
+                                " from Item i" +
+                                " where i.itemGender = :gender", ItemResponseDto.class)
+                .setParameter("gender", Gender.MALE)
+                .getResultList();
+    }
+
+    public List<ItemResponseDto> findWomenCategory(){
+        return em.createQuery(
+                        "select new com.capstone.dto.ItemResponseDto(i.id, i.image, i.itemName, i.price, i.company)" +
+                                " from Item i" +
+                                " where i.itemGender = :gender", ItemResponseDto.class)
+                .setParameter("gender", Gender.FEMALE)
+                .getResultList();
+    }
+
+    public List<ItemResponseDto> findSearchedItems(String content) {
+        return em.createQuery(
+                        "SELECT NEW com.capstone.dto.ItemResponseDto(i.id, i.image, i.itemName, i.price, i.company)" +
+                                " FROM Item i" +
+                                " WHERE i.itemName LIKE :content OR i.company LIKE :content", ItemResponseDto.class)
+                .setParameter("content", '%'+content+'%')
+                .getResultList();
     }
 }
--- src/main/java/com/capstone/service/ItemService.java
+++ src/main/java/com/capstone/service/ItemService.java
@@ -2,11 +2,13 @@ package com.capstone.service;
 
 import com.capstone.domain.Item.Item;
 import com.capstone.dto.ItemDetailDto;
+import com.capstone.dto.ItemResponseDto;
 import com.capstone.repository.ItemRepository;
 import com.capstone.repository.queryrepository.ItemQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,8 +17,35 @@ import java.util.Optional;
 public class ItemService {
 
     private final ItemQueryRepository itemQueryRepository;
+    private final ItemRepository itemRepository;
 
     public ItemDetailDto getItemDetails(long itemId) {
         return itemQueryRepository.findItemDetailDtos(itemId);
+    }
+
+    public List<ItemResponseDto> findAll() {
+        List<Item> items = itemRepository.findAll();
+        List<ItemResponseDto> result = new ArrayList<>();
+        for (Item item : items) {
+            ItemResponseDto itemResponseDto = new ItemResponseDto();
+            itemResponseDto.setId(item.getId());
+        }
+
+        return result;
+    }
+
+    public List<ItemResponseDto> searchItem(String content) {
+        List<ItemResponseDto> list = new ArrayList<>();
+        List<Item> items = itemRepository.findByItemNameContaining(content);
+        for (Item item : items) {
+            ItemResponseDto itemResponseDto = new ItemResponseDto();
+            itemResponseDto.setId(item.getId());
+            itemResponseDto.setItemName(item.getItemName());
+            itemResponseDto.setCompany(item.getCompany());
+            itemResponseDto.setPrice(item.getPrice());
+            itemResponseDto.setImage(item.getImage());
+            list.add(itemResponseDto);
+        }
+        return list;
     }
 }
