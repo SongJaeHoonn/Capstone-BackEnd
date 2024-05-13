efb9be13f57145822c164fa58dacebbdbbf6834e
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-05-13 21:30:02 +0900

    feat: 찜 중복 방지 기능 구현

--- src/main/java/com/capstone/service/CartService.java
+++ src/main/java/com/capstone/service/CartService.java
@@ -5,6 +5,9 @@ import com.capstone.domain.CartItem;
 import com.capstone.domain.Item.Item;
 import com.capstone.domain.Member;
 import com.capstone.dto.MemberSessionDto;
+import com.capstone.exception.ErrorCode;
+import com.capstone.exception.ErrorException;
+import com.capstone.exception.ErrorResponse;
 import com.capstone.repository.CartRepository;
 import com.capstone.repository.ItemRepository;
 import com.capstone.repository.MemberRepository;
@@ -12,6 +15,8 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.capstone.SessionFactory.SESSION_KEY;
 
 @Service
 @RequiredArgsConstructor
@@ -27,13 +32,16 @@ public class CartService {
         Cart cart = getCurrentCart(session);
 
         CartItem cartItem = CartItem.createCartItem(cart, item);
+        if(cart.getCartItems().contains(cartItem)){
+            throw ErrorException.type(ErrorCode.ALREADY_IN_CART);
+        }
         cart.addCartItem(cartItem);
 
         cartRepository.save(cart);
     }
 
     private Cart getCurrentCart(HttpSession session) {
-        MemberSessionDto getSession = (MemberSessionDto) session.getAttribute("SESSION_KEY");
+        MemberSessionDto getSession = (MemberSessionDto) session.getAttribute(SESSION_KEY);
         Member member = memberRepository.findById(getSession.getId()).orElseThrow(() -> new IllegalArgumentException("No Cart"));
         return member.getCart();
     }
