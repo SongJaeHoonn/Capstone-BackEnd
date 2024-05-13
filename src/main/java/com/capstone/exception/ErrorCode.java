f0e2fe697f314c557bb079744dd767d81150058c
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-05-13 21:30:42 +0900

    feat: 이미 찜한 상품 오류코드 추가

--- src/main/java/com/capstone/exception/ErrorCode.java
+++ src/main/java/com/capstone/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     WRONG_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다"),
     USER_ALREADY_JOIN(HttpStatus.UNAUTHORIZED, "이미 가입된 회원입니다"),
 
-    FAILED_WRITE_REVIEW(HttpStatus.UNAUTHORIZED, "리뷰 작성에 실패했습니다");
+    FAILED_WRITE_REVIEW(HttpStatus.UNAUTHORIZED, "리뷰 작성에 실패했습니다"),
+    ALREADY_IN_CART(HttpStatus.BAD_REQUEST, "이미 찜한 상품입니다");
 
     private final HttpStatus status;
     private final String message;
