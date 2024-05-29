2d802367fb90645a67826e484a48c401c578ad9e
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-05-29 17:06:41 +0900

    fix: CORS 재설정

--- src/main/java/com/capstone/config/WebMvcConfig.java
+++ src/main/java/com/capstone/config/WebMvcConfig.java
@@ -9,8 +9,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost:8081")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
                 .allowedHeaders("*");
     }
 }
