f7085fc22c0e5333a41b74e712a109eab425082b
JaeHoonSong <128021502+SongJaeHoonn@users.noreply.github.com>
2024-05-30 18:14:04 +0900

    fix: header 제거

--- src/main/java/com/capstone/config/WebMvcConfig.java
+++ src/main/java/com/capstone/config/WebMvcConfig.java
@@ -9,9 +9,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081")
+                .allowedOrigins("http://192.168.45.187:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true)
-                .allowedHeaders("*");
+                .allowCredentials(true);
     }
 }
